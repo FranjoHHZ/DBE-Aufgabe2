478b6cd5dd74bf238728b641c2c870478e576219
FranjoHHZ <167630679+FranjoHHZ@users.noreply.github.com>
2025-01-05 11:16:25 +0100

    Update Unit_download.py

--- src/Unit_download.py
+++ src/Unit_download.py
@@ -7,19 +7,21 @@ def download():
     y = mnist.target
     return (X, y)
 
-class Normalize:
+class Normalize(object): 
     def normalize(self, X_train, X_test):
         self.scaler = MinMaxScaler()
         X_train = self.scaler.fit_transform(X_train)
-        X_test = self.scaler.transform(X_test)
-        return (X_train, X_test)
+        X_test  = self.scaler.transform(X_test)
+        return (X_train, X_test) 
+    
+    def inverse(self, X_train, X_val, X_test):
+        X_train = self.scaler.inverse_transform(X_train)
+        X_test  = self.scaler.inverse_transform(X_test)
+        return (X_train, X_test)   
 
-    def inverse(self, X):
-        return self.scaler.inverse_transform(X)
-
-def split(X, y, split_ratio):
-    X_train = X[:split_ratio]
-    y_train = y[:split_ratio]
-    X_test = X[split_ratio:]
-    y_test = y[split_ratio:]
-    return (X_train, y_train, X_test, y_test)
+def split(X,y, splitRatio):
+    X_train = X[:splitRatio]
+    y_train = y[:splitRatio]
+    X_test = X[splitRatio:]
+    y_test = y[splitRatio:]
+    return (X_train, y_train, X_test, y_test)  
